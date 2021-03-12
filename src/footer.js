7622fdd0436b3a621ff7775c4b6be887884bfa66
Kisseime TEVOT <ktevot@gmail.com>
2021-03-12 18:33:17 +0100

    panier_app

--- src/footer.js
+++ src/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = (props) => {
 	return (
-		<div className="container-fluid position-absolute bottom-0 left-0 right-0">
+		<div className="container-fluid">
 			<div className="row">
 				<div className="col-12 py-3">
 					<div>
